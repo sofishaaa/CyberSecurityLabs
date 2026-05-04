a79de0440761023175caf16787b36840f2475bfd
sofiagunkalo <sofiikahunkalo@gmail.com>
2026-05-04 11:44:57 +0300

    lab1 task6: fixed CORS with mode1 and cors middleware

--- lab1/trustco/server.js
+++ lab1/trustco/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+app.use(cors());
+
 app.use(express.static('.'));
 
 app.get('/api/messages', (req, res) => {
